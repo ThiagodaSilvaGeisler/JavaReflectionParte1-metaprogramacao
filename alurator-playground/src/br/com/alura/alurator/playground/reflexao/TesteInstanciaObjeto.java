e81f3a654f9a4fa630c9b631a552079ff664af9d
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-09 14:25:56 -0300

    Três forma de criar um objeto do tipo Class

--- alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjeto.java
+++ alurator-playground/src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjeto.java
@@ -5,7 +5,13 @@ import br.com.alura.alurator.playground.controle.Controle;
 public class TesteInstanciaObjeto {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-	
+		Class<Controle> controleClasse1	= Controle.class;
+		
+		Controle controle = new Controle();
+		Class<? extends Controle> controleClasse2 = controle.getClass();
+		
+		Class<?> controleClasse3 = Class.forName("br.com.alura.alurator.playground.controle.Controle");
+
 	}
 
 }
