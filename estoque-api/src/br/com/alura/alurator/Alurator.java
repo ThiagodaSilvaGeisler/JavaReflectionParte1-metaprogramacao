e7757fe8bec5a52d09edb55deb9709d06f80cec5
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-10 08:01:47 -0300

    Usando Class.forName

--- estoque-api/src/br/com/alura/alurator/Alurator.java
+++ estoque-api/src/br/com/alura/alurator/Alurator.java
@@ -2,11 +2,29 @@ package br.com.alura.alurator;
 
 public class Alurator {
 	
+	
+	private String pacoteBase;
+	
+	public Alurator(String pacoteBase) {
+		this.pacoteBase = pacoteBase;
+	}
+	
+	
+	
 	public Object executa(String url) {
+		// processa a requisicao executando o método da classe em questãp
 		
 		String[] partesUrl = url.replaceFirst("/", "").split("/");
 		
 		String nomeControle = partesUrl[0] + "Controller";
+		
+		try {
+			Class.forName(pacoteBase + nomeControle);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
 		
 		return null;
 	}
