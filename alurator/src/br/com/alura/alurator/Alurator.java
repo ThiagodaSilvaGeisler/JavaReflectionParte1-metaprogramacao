7d103a830605253db6787f8eb43d5c9a319bc4c8
Thiago da Silva Geisler <contato.thiagogeisler@gmail.com>
2022-05-10 11:28:32 -0300

    Ajustando String url, informando pacote base pelo construtor de Alurator e instancinado um objeto do tipo PordutoController a partir de Class<?> classeControle

--- alurator/src/br/com/alura/alurator/Alurator.java
+++ alurator/src/br/com/alura/alurator/Alurator.java
@@ -2,10 +2,36 @@ package br.com.alura.alurator;
 
 public class Alurator {
 	
-	public Object executa(String url) {
-		// TODO - processa a requisicao executando o metodo
-		// da classe em questao
-		
-		return null;
+	
+	private String pacoteBase;
+	
+	public Alurator(String pacoteBase) {
+		this.pacoteBase = pacoteBase;
 	}
+	
+	
+	
+	public Object executa(String url) {
+		// processa a requisicao executando o método da classe em questãp
+		
+		// produto lista
+		
+		String[] partesUrl = url.replaceFirst("/", "").split("/");
+		
+		String nomeControle = Character.toUpperCase(partesUrl[0].charAt(0)) + partesUrl[0].substring(1) + "Controller";
+		
+		try {
+			Class <?> classeControle = Class.forName(pacoteBase + nomeControle);
+			Object instanciaControle = classeControle.newInstance();
+			System.out.println(instanciaControle);
+			return null;
+		} catch (ClassNotFoundException| InstantiationException | IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		
+	}
+
 }
